3af1ffe1ffb42cb1140b7556c1c2cea6772b39ed
Luis Eduardo <83380287+luismedeis91@users.noreply.github.com>
2025-09-07 07:14:09 -0300

    Interface jogo e classe

---
+++ Steam/src/Dados_Jogos.java
@@ -0,0 +1,13 @@
+public interface Dados_Jogos {
+
+    default void exibirDetalhes() {
+        System.out.printf("Jogo: %s | Id: %d | Preço: %d%n",
+                getNome(), getID_Jogo(), getPreco());
+    }
+
+    String getNome();
+
+    int getID_Jogo();
+
+    int getPreco();
+}
---
+++ Steam/src/jogo.java
@@ -0,0 +1,50 @@
+public class jogo implements Dados_Jogos {
+    private int ID_Jogo;
+    private int Preco;
+    private String Marcador;
+    private String Descricao;
+    private String Nome;
+
+    public jogo(int ID_Jogo, String nome, int Preco) {
+        this.ID_Jogo = ID_Jogo;
+        this.Nome = nome;
+        this.Preco = Preco;
+    }
+
+    public void setMarcador(String Marcador) {
+        this.Marcador = Marcador;
+    }
+
+    public void setDescricao(String Descricao) {
+        this.Marcador = Descricao;
+    }
+
+    public void mudarNome(String nome) {
+        this.Nome = nome;
+    }
+
+    public void mudarPreco(int Preco) {
+        this.Preco = Preco;
+    }
+
+    public int getID_Jogo() {
+        return ID_Jogo;
+    }
+
+    public int getPreco() {
+        return Preco;
+    }
+
+    public String getMarcador() {
+        return Marcador;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+}
